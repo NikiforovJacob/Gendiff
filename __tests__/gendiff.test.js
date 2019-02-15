dacced4b6209cbb68300807017703eb084245a82
Jacob_N <nikiforov.jacob@yandex.ru>
2019-02-15 16:53:50 +0700

    step 2: corrections on the mentor comments #4

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -1,25 +1,13 @@
 import fs from 'fs';
 import gendiff from '../src';
 
-const readFile = pathFile => fs.readFileSync(pathFile, { encoding: 'utf-8' });
-
-test('Compares two JSON', () => {
-  const pathToFile1 = '__tests__/__fixtures__/before.json';
-  const pathToFile2 = '__tests__/__fixtures__/after.json';
-  const contentCheck = readFile('__tests__/__fixtures__/check.txt');
-  expect(gendiff(pathToFile1, pathToFile2)).toMatch(contentCheck);
-});
-
-test('Compares two YAML', () => {
-  const pathToFile1 = '__tests__/__fixtures__/before.yml';
-  const pathToFile2 = '__tests__/__fixtures__/after.yml';
-  const contentCheck = readFile('__tests__/__fixtures__/check.txt');
-  expect(gendiff(pathToFile1, pathToFile2)).toMatch(contentCheck);
-});
-
-test('Compares two ini', () => {
-  const pathToFile1 = '__tests__/__fixtures__/before.ini';
-  const pathToFile2 = '__tests__/__fixtures__/after.ini';
-  const contentCheck = readFile('__tests__/__fixtures__/check.txt');
-  expect(gendiff(pathToFile1, pathToFile2)).toMatch(contentCheck);
-});
+test.each([
+  ['__tests__/__fixtures__/before.json', '__tests__/__fixtures__/after.json', '__tests__/__fixtures__/check.txt'],
+  ['__tests__/__fixtures__/before.ini', '__tests__/__fixtures__/after.ini', '__tests__/__fixtures__/check.txt'],
+  ['__tests__/__fixtures__/before.yml', '__tests__/__fixtures__/after.yml', '__tests__/__fixtures__/check.txt']
+  ])('Compares two JSON, YAML, ini',
+  (pathToFile1, pathToFile2, pathToCheck) => {
+    const readFile = pathFile => fs.readFileSync(pathFile, { encoding: 'utf-8' });
+    expect(gendiff(pathToFile1, pathToFile2)).toMatch(readFile(pathToCheck));
+  }
+);
--- src/index.js
+++ src/index.js
@@ -6,26 +6,22 @@ const newLine = '\n';
 const gendiff = (pathToFileBefore, pathToFileAfter) => {
   const contentBefore = parse(pathToFileBefore);
   const contentAfter = parse(pathToFileAfter);
-  const reduceAfterToBefore = (acc, currentKey) => {
+
+  const unionContentKeys = Array.from(new Set(Object.keys(contentAfter).concat(Object.keys(contentBefore))));
+  const compare = (currentKey) => {
     if (!_.has(contentBefore, currentKey)) {
-      return `${acc}  + ${currentKey}: ${contentAfter[currentKey]}${newLine}`;
+      return `  + ${currentKey}: ${contentAfter[currentKey]}${newLine}`;
     }
     if (contentBefore[currentKey] === contentAfter[currentKey]) {
-      return `${acc}    ${currentKey}: ${contentAfter[currentKey]}${newLine}`;
+      return `    ${currentKey}: ${contentAfter[currentKey]}${newLine}`;
     }
-    return `${acc}  + ${currentKey}: ${contentAfter[currentKey]}${newLine}  - ${currentKey}: ${contentBefore[currentKey]}${newLine}`;
-  };
-  const genAfterToBefore = Object.keys(contentAfter).reduce(reduceAfterToBefore, '');
-
-  const reduceBeforeToAfter = (acc, currentKey) => {
-    if (!_.has(contentAfter, currentKey)) {
-      return `${acc}  - ${currentKey}: ${contentBefore[currentKey]}${newLine}`;
+    if (_.has(contentAfter, currentKey) && contentBefore[currentKey] !== contentAfter[currentKey]) {
+      return `  + ${currentKey}: ${contentAfter[currentKey]}${newLine}  - ${currentKey}: ${contentBefore[currentKey]}${newLine}`;
     }
-    return acc;
+    return `  - ${currentKey}: ${contentBefore[currentKey]}${newLine}`;
   };
-
-  const genBeforeToAfter = Object.keys(contentBefore).reduce(reduceBeforeToAfter, genAfterToBefore);
-  return `{${newLine}${genBeforeToAfter.slice(0, -1)}${newLine}}`;
+  const gen = unionContentKeys.map(compare);
+  return `{${newLine}${gen.join('').slice(0, -1)}${newLine}}`;
 };
 
 export default gendiff;
