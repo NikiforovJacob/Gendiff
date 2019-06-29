648e13e616e758789c8911eb5caa12cdbfaa6f77
Jacob Nikiforov <nikiforov.jacob@yandex.ru>
2019-06-29 10:44:25 +0700

    refactoring

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -1,13 +1,22 @@
 import fs from 'fs';
 import gendiff from '../src';
 
+const subjectJson = ['__tests__/__fixtures__/flat/before.json', '__tests__/__fixtures__/flat/after.json'];
+const subjectIni = ['__tests__/__fixtures__/flat/before.ini', '__tests__/__fixtures__/flat/after.ini'];
+const subjectYaml = ['__tests__/__fixtures__/flat/before.yml', '__tests__/__fixtures__/flat/after.yml'];
+
+const subjectJsonTree = ['__tests__/__fixtures__/nesting/beforeTree.json', '__tests__/__fixtures__/nesting/afterTree.json'];
+const subjectIniTree = ['__tests__/__fixtures__/nesting/beforeTree.ini', '__tests__/__fixtures__/nesting/afterTree.ini'];
+const subjectYamlTree = ['__tests__/__fixtures__/nesting/beforeTree.yml', '__tests__/__fixtures__/nesting/afterTree.yml'];
+
+
 test.each([
-  ['__tests__/__fixtures__/flat/before.json', '__tests__/__fixtures__/flat/after.json', '__tests__/__fixtures__/flat/check.txt'],
-  ['__tests__/__fixtures__/flat/before.ini', '__tests__/__fixtures__/flat/after.ini', '__tests__/__fixtures__/flat/check.txt'],
-  ['__tests__/__fixtures__/flat/before.yml', '__tests__/__fixtures__/flat/after.yml', '__tests__/__fixtures__/flat/check.txt'],
-  ['__tests__/__fixtures__/nesting/beforeTree.json', '__tests__/__fixtures__/nesting/afterTree.json', '__tests__/__fixtures__/nesting/checkTree.txt'],
-  ['__tests__/__fixtures__/nesting/beforeTree.ini', '__tests__/__fixtures__/nesting/afterTree.ini', '__tests__/__fixtures__/nesting/checkTree.txt'],
-  ['__tests__/__fixtures__/nesting/beforeTree.yml', '__tests__/__fixtures__/nesting/afterTree.yml', '__tests__/__fixtures__/nesting/checkTree.txt'],
+  [...subjectJson, '__tests__/__fixtures__/flat/check.txt'],
+  [...subjectIni, '__tests__/__fixtures__/flat/check.txt'],
+  [...subjectYaml, '__tests__/__fixtures__/flat/check.txt'],
+  [...subjectJsonTree, '__tests__/__fixtures__/nesting/checkTree.txt'],
+  [...subjectIniTree, '__tests__/__fixtures__/nesting/checkTree.txt'],
+  [...subjectYamlTree, '__tests__/__fixtures__/nesting/checkTree.txt'],
 ])('Compares two JSON, YAML, ini, conclusions in nesting',
   (pathToFile1, pathToFile2, pathToCheck) => {
     const readFile = pathFile => fs.readFileSync(pathFile, { encoding: 'utf-8' });
@@ -15,12 +24,12 @@ test.each([
   });
 
 test.each([
-  ['__tests__/__fixtures__/flat/before.json', '__tests__/__fixtures__/flat/after.json', '__tests__/__fixtures__/flat/checkPlain.txt'],
-  ['__tests__/__fixtures__/flat/before.ini', '__tests__/__fixtures__/flat/after.ini', '__tests__/__fixtures__/flat/checkPlain.txt'],
-  ['__tests__/__fixtures__/flat/before.yml', '__tests__/__fixtures__/flat/after.yml', '__tests__/__fixtures__/flat/checkPlain.txt'],
-  ['__tests__/__fixtures__/nesting/beforeTree.json', '__tests__/__fixtures__/nesting/afterTree.json', '__tests__/__fixtures__/nesting/checkTreePlain.txt'],
-  ['__tests__/__fixtures__/nesting/beforeTree.ini', '__tests__/__fixtures__/nesting/afterTree.ini', '__tests__/__fixtures__/nesting/checkTreePlain.txt'],
-  ['__tests__/__fixtures__/nesting/beforeTree.yml', '__tests__/__fixtures__/nesting/afterTree.yml', '__tests__/__fixtures__/nesting/checkTreePlain.txt'],
+  [...subjectJson, '__tests__/__fixtures__/flat/checkPlain.txt'],
+  [...subjectIni, '__tests__/__fixtures__/flat/checkPlain.txt'],
+  [...subjectYaml, '__tests__/__fixtures__/flat/checkPlain.txt'],
+  [...subjectJsonTree, '__tests__/__fixtures__/nesting/checkTreePlain.txt'],
+  [...subjectIniTree, '__tests__/__fixtures__/nesting/checkTreePlain.txt'],
+  [...subjectYamlTree, '__tests__/__fixtures__/nesting/checkTreePlain.txt'],
 ])('Compares two JSON, YAML, ini, conclusions in plain',
   (pathToFile1, pathToFile2, pathToCheck) => {
     const readFile = pathFile => fs.readFileSync(pathFile, { encoding: 'utf-8' });
@@ -28,12 +37,12 @@ test.each([
   });
 
 test.each([
-  ['__tests__/__fixtures__/flat/before.json', '__tests__/__fixtures__/flat/after.json', '__tests__/__fixtures__/flat/checkJSON.txt'],
-  ['__tests__/__fixtures__/flat/before.ini', '__tests__/__fixtures__/flat/after.ini', '__tests__/__fixtures__/flat/checkJSON.txt'],
-  ['__tests__/__fixtures__/flat/before.yml', '__tests__/__fixtures__/flat/after.yml', '__tests__/__fixtures__/flat/checkJSON.txt'],
-  ['__tests__/__fixtures__/nesting/beforeTree.json', '__tests__/__fixtures__/nesting/afterTree.json', '__tests__/__fixtures__/nesting/checkTreeJSON.txt'],
-  ['__tests__/__fixtures__/nesting/beforeTree.ini', '__tests__/__fixtures__/nesting/afterTree.ini', '__tests__/__fixtures__/nesting/checkTreeJSON.txt'],
-  ['__tests__/__fixtures__/nesting/beforeTree.yml', '__tests__/__fixtures__/nesting/afterTree.yml', '__tests__/__fixtures__/nesting/checkTreeJSON.txt'],
+  [...subjectJson, '__tests__/__fixtures__/flat/checkJSON.txt'],
+  [...subjectIni, '__tests__/__fixtures__/flat/checkJSON.txt'],
+  [...subjectYaml, '__tests__/__fixtures__/flat/checkJSON.txt'],
+  [...subjectJsonTree, '__tests__/__fixtures__/nesting/checkTreeJSON.txt'],
+  [...subjectIniTree, '__tests__/__fixtures__/nesting/checkTreeJSON.txt'],
+  [...subjectYamlTree, '__tests__/__fixtures__/nesting/checkTreeJSON.txt'],
 ])('Compares two JSON, YAML, ini, conclusions in JSON AST',
   (pathToFile1, pathToFile2, pathToCheck) => {
     const readFile = pathFile => fs.readFileSync(pathFile, { encoding: 'utf-8' });
--- src/buildAst.js
+++ src/buildAst.js
@@ -1,41 +1,35 @@
 import _ from 'lodash';
 
+const typesDiffs = [
+  {
+    check: (b, a, key) => _.isObject(b[key]) && _.isObject(a[key]),
+    build: (b, a, buildAst) => ({ type: 'nesting', children: buildAst(b, a) }),
+  },
+  {
+    check: (b, a, key) => !_.has(b, key) && _.has(a, key),
+    build: (b, a) => ({ type: 'added', valueAfter: a }),
+  },
+  {
+    check: (b, a, key) => _.has(b, key) && !_.has(a, key),
+    build: b => ({ type: 'deleted', valueBefore: b }),
+  },
+  {
+    check: (b, a, key) => b[key] === a[key],
+    build: b => ({ type: 'unchanged', valueBefore: b }),
+  },
+  {
+    check: (b, a, key) => b[key] !== a[key],
+    build: (b, a) => ({ type: 'changed', valueBefore: b, valueAfter: a }),
+  },
+];
+
 const buildAst = (before, after) => {
   const allKeys = _.union(Object.keys(before), Object.keys(after));
-
-  const typesActions = [
-    {
-      type: 'nesting',
-      check: (b, a, key) => _.isObject(b[key]) && _.isObject(a[key]),
-      build: (b, a, key) => ({ type: 'nesting', children: buildAst(b[key], a[key]) }),
-    },
-    {
-      type: 'added',
-      check: (b, a, key) => !_.has(b, key) && _.has(a, key),
-      build: (b, a, key) => ({ type: 'added', valueAfter: a[key] }),
-    },
-    {
-      type: 'deleted',
-      check: (b, a, key) => _.has(b, key) && !_.has(a, key),
-      build: (b, a, key) => ({ type: 'deleted', valueBefore: b[key] }),
-    },
-    {
-      type: 'unchanged',
-      check: (b, a, key) => b[key] === a[key],
-      build: (b, a, key) => ({ type: 'unchanged', valueBefore: b[key] }),
-    },
-    {
-      type: 'changed',
-      check: (b, a, key) => b[key] !== a[key],
-      build: (b, a, key) => ({ type: 'changed', valueBefore: b[key], valueAfter: a[key] }),
-    },
-  ];
-
   const iterAst = (key) => {
     const emptyNode = { name: key };
-    const getTypesAction = (b, a) => typesActions.find(({ check }) => check(b, a, key));
-    const result = getTypesAction(before, after).build(before, after, key);
-    return { ...emptyNode, ...result };
+    const getTypesAction = (b, a) => typesDiffs.find(({ check }) => check(b, a, key));
+    const bodyNodes = getTypesAction(before, after).build(before[key], after[key], buildAst);
+    return { ...emptyNode, ...bodyNodes };
   };
   return allKeys.map(iterAst);
 };
